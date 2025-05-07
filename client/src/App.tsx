2d0a246eeb84170c67760c92c1bf53ee4fe4cf46
bahenawilliamje <41832407-bahenawilliamje@users.noreply.replit.com>
2025-05-07 16:53:18 +0000

    Restored to 'a94fde2b6043ba6af690b45dfb5e1814719d0aeb'
    Replit-Restored-To: a94fde2b6043ba6af690b45dfb5e1814719d0aeb

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,7 +20,6 @@ import MyCommissionsPage from "@/pages/my-commissions-page";
 import ReservationRequestsPage from "@/pages/reservation-requests-page";
 import NotificationsPage from "@/pages/notifications-page";
 import CouponsPage from "@/pages/coupons-page";
-import EditTripPage from "@/pages/edit-trip-page";
 import { UsersPage } from "@/components/users/users-page";
 
 function Router() {
@@ -34,7 +33,6 @@ function Router() {
       <ProtectedRoute path="/reservation-requests" component={ReservationRequestsPage} />
       <ProtectedRoute path="/notifications" component={NotificationsPage} />
       <ProtectedRoute path="/coupons" component={CouponsPage} />
-      <ProtectedRoute path="/edit-trip/:id" component={EditTripPage} />
       <Route path="/auth" component={AuthPage} />
       <Route path="/register/:token" component={RegisterPage} />
       <Route path="/reservation-details" component={ReservationDetails} />
